c71626a6b6ac9baaebb6a9191d421fb7e0cde5ad
Kaan Kani İnce <kkince@newmind.ai>
2024-09-10 11:08:09 +0300

    fix

--- model/adminModel.js
+++ model/adminModel.js
@@ -9,10 +9,9 @@ const authenticateAdmin = async (username, password) => {
         return isMatch ? user : null;
     }
     return null;
-
 };
 
- const getAdminByUsername = async (username) => {
+const getAdminByUsername = async (username) => {
     try {
         const result = await sql`SELECT * FROM admin WHERE username = ${username}`;
         return result?.rows[0];
@@ -23,67 +22,65 @@ const authenticateAdmin = async (username, password) => {
 };
 
 const getTeachers = async (req, res) => {
-    return sql("SELECT * FROM teacher ORDER BY id");
+    return sql`SELECT * FROM teacher ORDER BY id`;
 }
 
 const getStudents = async (req, res) => {
-    return sql(`
+    return sql`
         SELECT s.*, c.grade, c.section
         FROM student s
-                 LEFT JOIN classes c ON s.class_id = c.id
+        LEFT JOIN classes c ON s.class_id = c.id
         ORDER BY s.id;
-    `);
+    `;
 }
 
 const getClassAssignments = async (req, res) => {
-    return sql(`
+    return sql`
         SELECT ca.*,
                t.fname,
                t.lname,
                c.grade,
                c.section
         FROM class_assignments ca
-                 JOIN
-             teacher t ON ca.teacher_id = t.id
-                 JOIN
-             classes c ON ca.class_id = c.id
-    `);
+        JOIN teacher t ON ca.teacher_id = t.id
+        JOIN classes c ON ca.class_id = c.id
+    `;
 }
 
 const addTeacher = async ({fname, lname, email, password, lesson, role_id}) => {
-    return sql("INSERT INTO teacher (fname, lname, email, password, lesson, role_id) VALUES ($1, $2, $3, $4, $5, $6)", [fname, lname, email, password, lesson, role_id]);
+    return sql`INSERT INTO teacher (fname, lname, email, password, lesson, role_id) VALUES (${fname}, ${lname}, ${email}, ${password}, ${lesson}, ${role_id})`;
 }
 
 const addStudent = async ({fname, lname, password, email, b_date, class_id, role_id}) => {
-    return sql("INSERT INTO student (fname, lname, password, email, b_date, class_id, role_id) VALUES ($1, $2, $3, $4, $5, $6, $7)", [fname, lname, password, email, b_date, class_id, role_id]);
+    return sql`INSERT INTO student (fname, lname, password, email, b_date, class_id, role_id) VALUES (${fname}, ${lname}, ${password}, ${email}, ${b_date}, ${class_id}, ${role_id})`;
 }
 
 const addClassAssignments = async ({teacher_id, class_id}) => {
-    return sql("INSERT INTO class_assignments (teacher_id, class_id) VALUES ($1, $2)", [teacher_id, class_id]);
+    return sql`INSERT INTO class_assignments (teacher_id, class_id) VALUES (${teacher_id}, ${class_id})`;
 }
 
 const deleteTeacher = async (id) => {
-    return sql("DELETE FROM teacher WHERE id = $1", [id])
+    return sql`DELETE FROM teacher WHERE id = ${id}`;
 }
 
 const deleteStudent = async (id) => {
-    return sql("DELETE FROM student WHERE id = $1", [id])
+    return sql`DELETE FROM student WHERE id = ${id}`;
 }
 
 const deleteClassAssignments = async (id) => {
-    return sql("DELETE FROM class_assignments WHERE id = $1", [id])
+    return sql`DELETE FROM class_assignments WHERE id = ${id}`;
 }
 
 const updateTeacher = async ({fname, lname, email, password, lesson, role_id, id}) => {
-    return sql("UPDATE teacher SET fname = $1, lname = $2, email = $3, password = $4, lesson = $5, role_id = $6 WHERE id = $7", [fname, lname, email, password, lesson, role_id, parseInt(id)]);
+    return sql`UPDATE teacher SET fname = ${fname}, lname = ${lname}, email = ${email}, password = ${password}, lesson = ${lesson}, role_id = ${role_id} WHERE id = ${parseInt(id)}`;
 }
 
 const updateStudent = async ({fname, lname, password, email, b_date, class_id, role_id, id}) => {
-    return sql("UPDATE student SET fname = $1, lname = $2, password = $3, email = $4, b_date = $5, class_id = $6, role_id = $7 WHERE id = $8", [fname, lname, password, email, b_date, class_id, role_id, parseInt(id)]);
+    return sql`UPDATE student SET fname = ${fname}, lname = ${lname}, password = ${password}, email = ${email}, b_date = ${b_date}, class_id = ${class_id}, role_id = ${role_id} WHERE id = ${parseInt(id)}`;
 }
 
 const updateClassAssignments = async ({teacher_id, class_id, id}) => {
-    return sql("UPDATE class_assignments SET teacher_id = $1, class_id = $2 WHERE id = $3", [teacher_id, class_id, parseInt(id)]);
+    return sql`UPDATE class_assignments SET teacher_id = ${teacher_id}, class_id = ${class_id} WHERE id = ${parseInt(id)}`;
 }
 
 module.exports = {
