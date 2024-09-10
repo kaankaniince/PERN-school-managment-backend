b72fb1eeb8d52f47b9a6cf591471e28d187c1e5a
Kaan Kani İnce <kkince@newmind.ai>
2024-09-10 10:35:54 +0300

    cors fix

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const dotenv = require('dotenv');
 
 const PORT = process.env.PORT || 5001;
 
-app.use(cors({
-    origin: 'https://pern-school-managment-frontend.vercel.app'
-}));
+app.use(cors());
 app.use(express.json());
 
 // app.use(authMiddleware) ?
